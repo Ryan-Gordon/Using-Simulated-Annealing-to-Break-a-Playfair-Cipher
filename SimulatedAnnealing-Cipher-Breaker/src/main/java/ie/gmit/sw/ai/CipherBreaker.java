abcca6260f9463640dfec47d0f91f5fc33c9a595
Ryan Gordon <ryangordon210@gmail.com>
2018-03-26 16:36:28 +0100

    Created a new maven project

---
+++ SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/CipherBreaker.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class CipherBreaker 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
