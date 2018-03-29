c3aafcce872ae2f67500480e5e457309eefd45c7
Ryan Gordon <ryangordon210@gmail.com>
2018-03-29 23:25:58 +0100

    Decrypting with a known cipher key

--- SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/CipherBreaker.java
+++ SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/CipherBreaker.java
@@ -1,13 +1,29 @@
 package ie.gmit.sw.ai;
 
+import ie.gmit.sw.ai.crypto.PlayfairCipher;
+
 /**
  * Hello world!
  *
  */
 public class CipherBreaker 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Throwable
     {
         System.out.println( "Hello World!" );
+        
+        FileHandler fh = new FileHandler();
+        System.out.println(fh.listFiles("./files"));
+        
+        PlayfairCipher play = new PlayfairCipher(fh.readFile("./files/the-hobbit-cp1.txt"));
+       
+
+        
+        
+        System.out.println(fh.readFile("./files/the-hobbit-cp1.txt"));
+        System.out.println(play.decrypt("THEQUICKBROWNFXMPDVLAZYGS"));
+        
     }
 }
+    
+
