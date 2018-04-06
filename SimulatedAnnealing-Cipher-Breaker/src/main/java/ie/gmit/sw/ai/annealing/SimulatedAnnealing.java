621cc6532eb6b01cc10b00c14a412c356a1c9cdd
Ryan Gordon <ryangordon210@gmail.com>
2018-04-06 14:56:05 +0100

    Created class for annealing

---
+++ SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/annealing/SimulatedAnnealing.java
@@ -0,0 +1,17 @@
+package ie.gmit.sw.ai.annealing;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import ie.gmit.sw.ai.crypto.PlayfairCipher;
+import ie.gmit.sw.ai.crypto.KeyHandler;
+
+public class SimulatedAnnealing {
+
+	
+	public SimulatedAnnealing() {
+		super();
+	}
+	
+
+}
