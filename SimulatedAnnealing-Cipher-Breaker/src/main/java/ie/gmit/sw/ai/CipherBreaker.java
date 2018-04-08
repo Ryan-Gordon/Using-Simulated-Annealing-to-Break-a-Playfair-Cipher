413a29fc783bcd746a611485fe16e5b75f955c3e
Ryan Gordon <ryangordon210@gmail.com>
2018-04-08 16:24:15 +0100

    Option 1 for menu : Solve with hobbit file
    Takes in the file hobbits chapter 1 and attempts to decrypt with simulated annealing

--- SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/CipherBreaker.java
+++ SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/CipherBreaker.java
@@ -14,24 +14,16 @@ public class CipherBreaker
 	
     public static void main( String[] args ) throws Throwable
     {
-        System.out.println( "Hello World!" );
-        
-        FileHandler fh = new FileHandler();
-        System.out.println(fh.listFiles("./files"));
+    	FileHandler fh = new FileHandler();
         
         PlayfairCipher playDecryption = new PlayfairCipher(fh.readFile("./files/the-hobbit-cp1.txt"));
-        
-        System.out.println(fh.readFile("./files/the-hobbit-cp1.txt"));
-        System.out.println(playDecryption.decrypt("THEQUICKBROWNFXMPDVLAZYGS"));
-        
-        PlayfairCipher playEncryption = new PlayfairCipher("Happy Days");
-        System.out.println(playEncryption.encrypt("THEQUICKBROWNFXMPDVLAZYGS"));
-        PlayfairCipher playEncryption2 = new PlayfairCipher(playEncryption.encrypt("THEQUICKBROWNFXMPDVLAZYGS"));
-        System.out.println(playEncryption2.decrypt("THEQUICKBROWNFXMPDVLAZYGS"));
         
         SimulatedAnnealing sa = new SimulatedAnnealing();
         sa.solve(playDecryption, 20);
         
+        //Present UI to user 
+        Menu menu = new Menu();
+        menu.showMenu();
         
         
     }
--- SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/Menu.java
+++ SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/Menu.java
@@ -1,7 +1,19 @@
 package ie.gmit.sw.ai;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import ie.gmit.sw.ai.annealing.SimulatedAnnealing;
+import ie.gmit.sw.ai.crypto.PlayfairCipher;
 
 public class Menu {
+	
+	private PlayfairCipher cipher;
+	private SimulatedAnnealing cipherSolver;
+    private FileHandler fh;
+    
+    private String key;
     
     
    
@@ -9,6 +21,32 @@ public class Menu {
 
 	
 	public Menu() {
+		super();
+		
+		this.fh = new FileHandler();
+		this.cipherSolver = new SimulatedAnnealing();
 	}
 
+
+	public void showMenu() throws Throwable {
+		Scanner sc = new Scanner(System.in);
+			// Main menu for user
+			System.out.println("--------------------------------------------");
+			System.out.println("Enter a number to select an option:");
+			System.out.println("1. Break cipher text with simulated annealing (Hobbit Chapter 1)?");
+			
+			int choice = sc.nextInt();
+			System.out.println("--------------------------------------------");
+			
+			switch (choice) {
+			case 1: 
+					//Decrypting the hobbit chapter 1 
+					cipher = new PlayfairCipher(fh.readFile("./files/the-hobbit-cp1.txt"));
+					cipherSolver = new SimulatedAnnealing();
+					System.out.print("Enter the temperature: ");
+					System.out.println(cipherSolver.solve(cipher, sc.nextInt()));
+		        break;
+			}//switch
+		
+	}
 }
