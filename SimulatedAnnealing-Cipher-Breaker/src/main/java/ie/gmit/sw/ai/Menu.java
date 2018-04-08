2d63f9b2a4c13c64a45b65ef00d56fd9bb83dc1e
Ryan Gordon <ryangordon210@gmail.com>
2018-04-08 16:49:14 +0100

    Options 3,4 and quit option
    Decrypting from plain text or file

--- SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/Menu.java
+++ SimulatedAnnealing-Cipher-Breaker/src/main/java/ie/gmit/sw/ai/Menu.java
@@ -8,6 +8,8 @@ import ie.gmit.sw.ai.annealing.SimulatedAnnealing;
 import ie.gmit.sw.ai.crypto.PlayfairCipher;
 
 public class Menu {
+	
+	private boolean menuFlag = false;
 	private PlayfairCipher cipher;
 	private SimulatedAnnealing cipherSolver;
     private FileHandler fh;
@@ -24,17 +26,21 @@ public class Menu {
 		
 		this.fh = new FileHandler();
 		this.cipherSolver = new SimulatedAnnealing();
+		this.menuFlag = true;
 	}
 
 
 	public void showMenu() throws Throwable {
 		Scanner sc = new Scanner(System.in);
+		while(menuFlag != false) {
 			// Main menu for user
 			System.out.println("--------------------------------------------");
 			System.out.println("Enter a number to select an option:");
 			System.out.println("1. Break cipher text with simulated annealing (Hobbit Chapter 1)?");
 			System.out.println("2. Break cipher text with simulated annealing (Choose a file)?");
-			
+			System.out.println("3. Decrypt cipher text from command prompt?");
+			System.out.println("4. Decrypt cipher text from a file?");
+			System.out.println("5. Quit?");
 			int choice = sc.nextInt();
 			System.out.println("--------------------------------------------");
 			
@@ -63,8 +69,51 @@ public class Menu {
 					System.out.print("Enter the temperature: ");
 					System.out.println(cipherSolver.solve(cipher, sc.nextInt()));
 		        break;
+			case 3:
+				
+				sc.nextLine();
+		        System.out.println("Enter a key:");
+		        key = sc.next();
+				System.out.print("Enter the cipher text to decrypt: ");
+				sc.nextLine();
+				String cipherText = sc.nextLine();
+				
+				try {
+					cipher = new PlayfairCipher(cipherText);
+				} catch (Throwable e) {
+					// TODO Auto-generated catch block
+					System.out.println("Error reading the text specified : "+ cipherText);
+				}
+				//System.out.println(text);
+				String result = cipher.decrypt(key);
+				
+				
+				System.out.println("Result " + result);
+				break;
+				
+			case 4 :
+				sc.nextLine();
+		        System.out.println("Enter a key:");
+		        key = sc.next();
+				System.out.print("Enter the name of the txt file you want to decrypt: ");
+				sc.nextLine();
+				
+				try {
+					cipher = new PlayfairCipher(fh.readFile(sc.nextLine()));
+				} catch (Throwable e) {
+					// TODO Auto-generated catch block
+					System.out.println("Error reading the file specified ");
+				}
 			
+				System.out.println("Result " + cipher.decrypt(key));
+				break;
+			
+			case 5:
+				System.out.println("Quitting application");
+				sc.close();
+				menuFlag = false;
+				break;
 			}//switch
-		
+		}
 	}
 }
